fb63f072a40fa3dc38b5197d83b58144f36a93d0
dnkato <diane.n.kato@gmail.com>
2019-09-13 13:27:58 -0700

    Created skeleton build

--- WOFClassLib/Player.cs
+++ WOFClassLib/Player.cs
@@ -10,53 +10,5 @@ namespace WOFClassLib
     public class Player
     {
 
-        private int totalMoney = 0;
-        private int roundMoney = 0;
-
-        public string Name { get; set; }
-        public int TotalMoney
-        {
-            get { return totalMoney; }
-        }
-
-        public int RoundMoney {
-            get { return roundMoney;  }
-        }
-
-        public Player(string name = "Player")
-        {
-            Name = name;
-            totalMoney = 0;
-            roundMoney = 0;
-        }
-
-        public int GuessLetter(char guess, Puzzle puzzle, int spinAmount=0)
-        {
-            int numLetters = puzzle.Guess(guess);
-            roundMoney += numLetters * spinAmount;
-            return numLetters;
-        }
-
-        public bool SolvePuzzle(string guess, Puzzle puzzle)
-        {
-            bool isSolved = puzzle.Solve(guess);
-            if (isSolved)
-            {
-                totalMoney += RoundMoney;
-            }
-            return isSolved;
-        }
-
-        public void NewRound()
-        {
-            roundMoney = 0;
-        }
-
-        public void NewGame()
-        {
-            totalMoney = 0;
-            roundMoney = 0;
-        }
-
     }
 }
--- WheelOfFortune/Program.cs
+++ WheelOfFortune/Program.cs
@@ -11,22 +11,6 @@ namespace WheelOfFortune
     {
         static void Main(string[] args)
         {
-            var puzzle = new Puzzle("Blues Clues");
-
-            var player = new Player();
-
-            Console.WriteLine($"{puzzle.Display}, Solved={puzzle.Solved}");
-
-            player.GuessLetter('D',puzzle);
-            Console.WriteLine($"{puzzle.Display}, Solved={puzzle.Solved}");
-
-            player.GuessLetter('L', puzzle);
-            Console.WriteLine($"{puzzle.Display}, Solved={puzzle.Solved}");
-
-            player.SolvePuzzle("Blues Clues", puzzle);
-            Console.WriteLine($"{puzzle.Display}, Solved={puzzle.Solved}");
-
-            Console.ReadKey();
         }
     }
 }
