32941db821a3f0b7956614a3a0323e38ccd79096
denzalereese <reesedenzale@gmail.com>
2019-09-16 14:36:29 -0700

    adds unit tests

--- WOFClassLib.Tests/PuzzleTests.cs
+++ WOFClassLib.Tests/PuzzleTests.cs
@@ -23,9 +23,73 @@ namespace WOFClassLib.Tests
             Assert.Equal(testDisplay2, puzzleDisplay2);
         }
 
-        public void TestGuess()
+        [Theory]
+        [InlineData("dog", new char[] {'d'}, "d--")]
+        [InlineData("dog", new char[] { 'o' }, "-o-")]
+        [InlineData("dog", new char[] { 'g', 'o' }, "-og")]
+        [InlineData("dog", new char[] { 'g', 'g' }, "--g")]
+        [InlineData("dogs and cats", new char[] { 'C', 'a', 'S', 'd' }, "d--s a-d ca-s")]
+        [InlineData("leap", new char[] { 'x', 'p' }, "---p")]
+        public void TestGuessUpdatesPuzzle(string phrase, char[] guesses, string expectedResultDisplay)
         {
-
+            Puzzle testPuzzle = new Puzzle(phrase);
+            foreach (char guess in guesses)
+            {
+                testPuzzle.Guess(guess);
+            }
+            Assert.Equal(expectedResultDisplay, testPuzzle.GetPuzzleDisplay());
         }
+
+        [Theory]
+        [InlineData("quest", 'z', 0)]
+        [InlineData("wolf", 'w', 1)]
+        [InlineData("little red riding hood", 'r', 2)]
+        public void TestGuessReturnsCorrectNumberOfMatches(string phrase, char guess, int expectedNumMatches)
+        {
+            Puzzle testPuzzle = new Puzzle(phrase);
+            int numberOfMatches = testPuzzle.Guess(guess);
+            Assert.Equal(expectedNumMatches, numberOfMatches);
+        }
+
+        [Theory]
+        [InlineData("Microsoft Azure", "microsoft azure", true)]
+        [InlineData("dogs", "cats", false)]
+        [InlineData("United States of America", "United States", false)]
+        public void TestSolvePuzzle(string phrase, string guess, bool expectedResult)
+        {
+            Puzzle testPuzzle = new Puzzle(phrase);
+            if(expectedResult == true)
+            {
+                Assert.True(testPuzzle.Solve(guess));
+                Assert.True(testPuzzle.IsSolved());
+            }
+            else
+            {
+                Assert.False(testPuzzle.Solve(guess));
+                Assert.False(testPuzzle.IsSolved());
+            } 
+        }
+
+        //[Theory]
+        //[InlineData("dogs   cats")]
+        //[InlineData("back 2 back")]
+        //[InlineData("yes/no")]
+        //public void TestInvalidPuzzleThrowsException(string invalidPhrase)
+        //{
+        //    Assert.Throws<ArgumentException>(
+        //        () => new Puzzle(invalidPhrase));
+        //}
+
+        [Theory]
+        [InlineData('$')]
+        [InlineData('1')]
+        [InlineData(' ')]
+        public void TestInvalidGuessThrowsException(char invalidGuess)
+        {
+            Puzzle testPuzzle = new Puzzle("test puzzle");
+            Assert.Throws<ArgumentException>(
+                () => testPuzzle.Guess(invalidGuess));
+        }
+
     }
 }
